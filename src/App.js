439c14e5b36df386ea89b247475dbe21639f00b7
Jessica Paczuski <jz.paczuski@gmail.com>
2016-05-04 13:57:34 +0200

    add shoot functionality in app

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ class App extends React.Component {
       laserPosition: Position,
       laserActivated: true
     });
-
+    
     if (this.state.laserPosition > 500) {
       this.setState({
         laserActivated: false
