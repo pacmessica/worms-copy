9d9b1c84854ad4f85de9de1b95206b1a7ad82cb3
Ashley-Gautama <AshleyGautama.com>
2016-05-05 11:19:18 +0200

    changed joining game buttons to show when full

--- src/components/GameListItemComponent.js
+++ src/components/GameListItemComponent.js
@@ -1,16 +1,67 @@
 import React from 'react';
-import FlatButton from 'material-ui/lib/flat-button';
 
 class GameListItemComponent extends React.Component {
   selectGame() {
     this.props.onClick(this.props.game);
   }
 
+  gameOpen() {
+    return !this.gameFinished() &&
+      this.props.game.winner === null &&
+      (this.props.game.playerOne === this.props.currentPlayer ||
+      this.props.game.playerTwo === null ||
+      this.props.game.playerTwo === this.props.currentPlayer);
+  }
+
+  gameAlreadyJoined() {
+    return !this.gameFinished() &&
+      this.props.game.winner === null &&
+      (this.props.game.playerOne === this.props.currentPlayer ||
+      this.props.game.playerTwo === this.props.currentPlayer);
+  }
+
+  gameFull() {
+    return !this.gameOpen();
+  }
+
+  gameFinished() {
+    return this.props.game.winner !== null;
+  }
+
+  playerOrYou(player) {
+    if (player === this.props.currentPlayer) {
+      return "You";
+    }
+    return player;
+  }
+
+  theWinner() {
+    if (this.gameFinished()) {
+      if (this.props.game.winner === "draw") {
+        return `Draw between ${this.playerOrYou(this.props.game.playerOne)} and ${this.playerOrYou(this.props.game.playerTwo)}`;
+      }
+      return `${this.playerOrYou(this.props.game.winner)} won`;
+    } else {
+      return "Nobody";
+    }
+  }
+
   render() {
     return (
       <li>
-        <FlatButton onClick={this.selectGame.bind(this)} label="Join" primary={true}/>
         Game by {this.props.game.playerOne}
+
+        { this.gameOpen() && !this.gameAlreadyJoined() &&
+          <button onClick={this.selectGame.bind(this)}>Join Game</button> }
+
+        { this.gameOpen() && this.gameAlreadyJoined() &&
+          <button onClick={this.selectGame.bind(this)}>Resume Game</button> }
+
+        { !this.gameFinished() && this.gameFull() &&
+          <span>(Full)</span> }
+
+        { this.gameFinished() &&
+          <span>({this.theWinner()})</span> }
       </li>
     );
   }
